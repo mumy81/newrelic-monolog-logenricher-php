1f2f64e94eb6d7f54ccdd8f983f130ee6e5a0b68
Muhammed Celik <muhammedcelik@gmail.com>
2022-08-29 06:28:17 +0300

    Convert for monolog v1

--- src/Formatter.php
+++ src/Formatter.php
@@ -4,28 +4,40 @@
  * Copyright [2019] New Relic Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
- * This file contains the abstract parent of the Formatter class for
- * the New Relic Monolog Enricher. This class implements all functionality
- * that is compatible with all Monolog API versions
+ * This file contains the Formatter class for the New Relic Monolog Enricher
+ * on Monolog API v1
+ *
+ * This class formats a Monolog record as a JSON object with a compatible
+ * timestamp, and any New Relic context information moved to the top-level.
+ * The resulting output is intended to be sent to New Relic Logs via a
+ * compatible log forwarder with New Relic plugin installed (see this
+ * project's README for links to available plugins).
  *
  * @author New Relic PHP <php-agent@newrelic.com>
  */
 
 namespace NewRelic\Monolog\Enricher;
 
-use Monolog\Logger;
-
-// phpcs:disable
-/*
- * This extension to the Monolog framework supports the same PHP versions
- * as the New Relic PHP Agent (>=5.3).  Older versions of PHP are only
- * compatible with Monolog v1, therefore, To accomodate Monolog v2's explicit
- * and required type annotations, some overridden methods must be implemented
- * both with compatible annotations for v2 and without for v1
+/**
+ * Formats record as a JSON object with transformations necessary for
+ * ingestion by New Relic Logs
  */
-if (Logger::API == 2) {
-    require_once dirname(__FILE__) . '/api2/Formatter.php';
-} else {
-    require_once dirname(__FILE__) . '/api1/Formatter.php';
+if (Logger::API == 1) {
+    class Formatter extends AbstractFormatter
+    {
+        /**
+         * Normalizes each record individually before JSON encoding the complete
+         * batch of records as a JSON array.
+         *
+         * @param array $records
+         * @return string
+         */
+        protected function formatBatchJson(array $records)
+        {
+            foreach ($records as $key => $record) {
+                $records[$key] = $this->normalize($record);
+            }
+            return $this->toJson($records, true);
+        }
+    }
 }
-// phpcs:enable
--- src/Handler.php
+++ src/Handler.php
@@ -4,28 +4,80 @@
  * Copyright [2019] New Relic Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
- * This file contains the abstract parent of the Handler class for
- * the New Relic Monolog Enricher. This class implements all functionality
- * that is compatible with all Monolog API versions
+ * This file contains the Handler class for Monolog API v1
+ *
+ * This class provides a largely self-contained solution for
+ * getting started with New Relic logging, including support
+ * for batching uploads.
  *
  * @author New Relic PHP <php-agent@newrelic.com>
  */
 
 namespace NewRelic\Monolog\Enricher;
 
+use Monolog\Formatter\FormatterInterface;
 use Monolog\Logger;
+use NewRelic\Monolog\Enricher\AbstractHandler;
 
-// phpcs:disable
-/*
- * This extension to the Monolog framework supports the same PHP versions
- * as the New Relic PHP Agent (>=5.3).  Older versions of PHP are only
- * compatible with Monolog v1, therefore, To accomodate Monolog v2's explicit
- * and required type annotations, some overridden methods must be implemented
- * both with compatible annotations for v2 and without for v1
- */
-if (Logger::API == 2) {
-    require_once dirname(__FILE__) . '/api2/Handler.php';
-} else {
-    require_once dirname(__FILE__) . '/api1/Handler.php';
+if (Logger::API == 1) {
+    class Handler extends AbstractHandler
+    {
+        /**
+         * Delegates upload of single record to send()
+         *
+         * @param array $record
+         */
+        protected function write(array $record)
+        {
+            $this->send($record["formatted"]);
+        }
+
+        /**
+         * Iterates over batched data, filtering out logs with levels lower
+         * than the constructed threshold. If applicable logs are found, they
+         * are formatted as a JSON array compatible with New Relic's batch Log
+         * ingest and delegated to sendBatch()
+         *
+         * @param array $record
+         */
+        public function handleBatch(array $records)
+        {
+            $level = $this->level;
+            $records = array_filter($records, function ($record) use ($level) {
+                return ($record['level'] >= $level);
+            });
+            if ($records) {
+                $this->sendBatch($this->getFormatter()->formatBatch($records));
+            }
+        }
+
+        /**
+         * Sets Handler's Formatter. Note: only
+         * NewRelic\Monolog\Enricher\Formatter is compatible.
+         *
+         * @param FormatterInterface $formatter
+         *
+         * @throws InvalidArgumentException If incompatible Formatter given
+         */
+        public function setFormatter(FormatterInterface $formatter)
+        {
+            if ($formatter instanceof Formatter) {
+                return parent::setFormatter($formatter);
+            }
+            throw new \InvalidArgumentException(
+                'NewRelic\Monolog\Enricher\Handler is only compatible with '
+                . 'NewRelic\Monolog\Enricher\Formatter'
+            );
+        }
+
+        /**
+         * Returns a New Relic Formatter initialized with API compatible values
+         *
+         * @return Formatter
+         */
+        protected function getDefaultFormatter()
+        {
+            return new Formatter(Formatter::BATCH_MODE_JSON, false);
+        }
+    }
 }
-// phpcs:enable
